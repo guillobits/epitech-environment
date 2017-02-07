a31cbac1b154ad5dfb90b48acdd2a0feacbf0282
CAUCHOIS Guillaume <guillaume.cauchois@epitech.eu>
2017-02-07 16:25:43 +0100

    Suppression de la confirmation automatique
    Problème avec la confirmation auto

--- sources/start_install.sh
+++ sources/start_install.sh
@@ -10,47 +10,47 @@ function start_install()
 	echo ""
 
 	echo " ## INSTALLATION DE TERMINATOR"
-	echo -e "Y\n" | sudo pacman -S terminator
+	sudo pacman -S terminator
 	echo ""
 
 	echo " ## INSTALLATION DE TREE"
-	echo -e "Y\n" | sudo pacman -S tree
+	sudo pacman -S tree
 	echo ""
 
 	echo " ## INSTALLATION DE GIT"
-	echo -e "Y\n" | sudo pacman -S git
+	sudo pacman -S git
 	echo ""
 
 	echo " ## INSTALLATION DE OPENSSH"
-	echo -e "Y\n" | sudo pacman -S openssh
+	sudo pacman -S openssh
 	echo ""
 
 	echo " ## INSTALLATION DE ZSH"
-	echo -e "Y\n" | sudo pacman -S zsh
+	sudo pacman -S zsh
 	echo ""
 
 	echo " ## INSTALLATION DE CURL"
-	echo -e "Y\n" | sudo pacman -S curl
+	sudo pacman -S curl
 	echo ""
 
 	echo " ## INSTALLATION DE WGET"
-	echo -e "Y\n" | sudo pacman -S wget
+	sudo pacman -S wget
 	echo ""
 
 	echo " ## INSTALLATION DE EMACS"
-	echo -e "Y\n" | sudo pacman -S emacs-nox
+	sudo pacman -S emacs-nox
 	echo ""
 
 	echo " ## INSTALLATION DE LIBNCURSES"
-	echo -e "Y\n" | sudo pacman -S ncurses
+	sudo pacman -S ncurses
 	echo ""
 
 	echo " ## INSTALLATION DE VALGRIND"
-	echo -e "Y\n" | sudo pacman -S valgrind
+	sudo pacman -S valgrind
 	echo ""
 
 	echo " ## INSTALLATION DE GDB"
-	echo -e "Y\n" | sudo pacman -S gdb
+	sudo pacman -S gdb
 	echo ""
 
 	echo " ## INSTALLATION DE BLIH"
@@ -59,19 +59,19 @@ function start_install()
 	echo ""
 
 	echo " ## INSTALLATION DE GCC"
-	echo -e "Y\n" | sudo pacman -S gcc
+	sudo pacman -S gcc
 	echo ""
 
 	echo " ## INSTALLATION DE CLANG"
-	echo -e "Y\n" | sudo pacman -S clang
+	sudo pacman -S clang
 	echo ""
 
 	echo " ## INSTALLATION DE PYTHON"
-	echo -e "Y\n" | sudo pacman -S python
+	sudo pacman -S python
 	echo ""
 
 	echo " ## INSTALLATION DE FILEZILLA"
-	echo -e "Y\n" | sudo pacman -S filezilla
+	sudo pacman -S filezilla
 	echo ""
 
 }
